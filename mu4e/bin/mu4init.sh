b6622a6ee9040e6816055cd06430151178090e9e
John Lord <john@lordsonline.org>
2023-11-18 09:28:08 -0600

    Added mu4e script

---
+++ mu4e/bin/mu4init.sh
@@ -0,0 +1,10 @@
+mbsync -a
+mu init --maildir=$HOME/.mail \
+        --my-address=john@lordsonline.org \
+        --my-address=john@lordpharmaconsulting.com \
+        --my-address=john@lordpharma.com \
+        --my-address=lordjd@mac.com \
+        --my-address=lordjd@me.com \
+        --my-address=lordjd@icloud.com \
+        --my-address=lordjd66@gmail.com
+mu index
